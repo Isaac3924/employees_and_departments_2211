95216b75d5cb8c9616719a71052a5fb9d7a3a130
Isaac Alter <i_alter@yahoo.com>
2023-01-13 09:31:51 -0700

    Add feat: Iteration 2 test suite in department_spec

---
+++ spec/department_spec.rb
@@ -0,0 +1,39 @@
+require './lib/department'
+require './lib/employee'
+
+RSpec.describe Department do
+  let(:customer_service){ Department.new( "Customer Service" ) }
+  
+  let(:bobbi){ Employee.new( {name: "Bobbi Jaeger", 
+                             age: "30", 
+                             salary: "100000"} ) }
+
+  let(:aaron){ Employee.new( {name: "Aaron Tanaka", 
+                             age: "25", 
+                             salary: "90000"} ) }
+  describe 'iteration2' do
+    it '#initializes' do  
+      expect(customer_service).to be_a(Department)
+    end
+
+    it 'has attributes' do  
+      expect(customer_service.name).to eq("Customer Service")
+      expect(customer_service.employees).to eq( [] )
+    end
+
+    it '#hire' do  
+      customer_service.hire(bobbi)
+      customer_service.hire(aaron)
+
+
+      expect(customer_service.employees).to eq([bobbi, aaron])
+    end
+
+    it '#expense' do
+      customer_service.expense(100)
+      customer_service.expense(25)
+
+      expect(customer_service.expenses).to eq(125)
+    end
+  end
+end
