436d849be3c17372f6efccc0f2e56992be9ac5bd
Isaac Alter <i_alter@yahoo.com>
2023-01-13 09:34:48 -0700

    Add feat: Department creation and #initialize with attributes

---
+++ lib/department.rb
@@ -0,0 +1,11 @@
+class Department
+    attr_reader :name,
+                :employees,
+                :expenses
+  
+    def initialize(name)
+      @name = name
+      @employees = []
+      @expenses = 0
+    end
+  end
